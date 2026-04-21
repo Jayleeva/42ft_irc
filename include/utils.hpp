f59686e0486ca1657aae1536b6ce2354d314717b
Marjo412 <marjorie_4.28@hotmail.com>
2026-04-21 18:28:02 +0200

    add error messages

--- include/utils.hpp
+++ include/utils.hpp
@@ -5,13 +5,17 @@
 #include <string>
 
 //Error messages
-#define ERR_PASSWDMISMATCH "Wrong Password"
+#define ERR_PASSWDMISMATCH "Password incorrect"
 #define ERR_NEEDPASS "Password required"
 #define ERR_CMD "Unknown command"
 #define ERR_ERRONEUSNICKNAME "Erroneus nickname"
-#define ERR_NICKNAMEINUSE "This Nickname is already used"
+#define ERR_NICKNAMEINUSE "Nickname is already in use"
 #define ERR_NEEDMOREPARAMS "Not enough parameters"
 #define ERR_REGISTRED "Already registered"
+#define ERR_USERONCHANNEL "User is already on channel"
+#define ERR_CHANNELISFULL "Cannot join channel : channel is full"
+#define ERR_BANNEDFROMCHAN "Cannot join channel : banned from channel"
+#define ERR_INVITEONLYCHAN "Cannot join channel : invite only channel"
 
 //parsing functions
 std::string getCommand(const std::string& msg);
