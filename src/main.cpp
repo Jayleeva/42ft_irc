fe3579fff194d38eb97fc5ad360e455cad0c7f2a
Cynthia Glardon <cy.glardon@gmail.com>
2026-06-17 15:33:50 +0200

    added protections and the address argument

--- src/main.cpp
+++ src/main.cpp
@@ -5,12 +5,6 @@
 
 #include "../include/Server.hpp"
 #include "../include/utils.hpp"
-// ports possibles pour IRC : 6665 - 6669, par defaut 6667
-
-// errno 22: EINVAL Invalid argument
-// EINVAL in bind():
-//    The socket is already bound to an address.
-//    addrlen is wrong, or addr is not a valid address for this socket's domain.
 
 bool g_running = true;
 
@@ -30,32 +24,74 @@ void runServer(Server *serv)
     }
 }
 
+int isAddressValid(std::string address)
+{
+    std::vector<std::string>	parsing;
+	std::string			        element;
+    std::stringstream 	        ss(address);
+
+	while (getline(ss, element, '.'))
+		parsing.push_back(element);
+    
+    if (parsing.size() != 4)
+        return (0);
+
+    for (std::vector<std::string>::iterator it = parsing.begin(); it != parsing.end(); it ++)
+    {
+        try
+        {
+            int tmp = ft_stoi(*it);
+            if (tmp < 0 || tmp > 255)
+                return (0);
+        }
+        catch(const std::exception& e)
+        {
+            return (0);
+        }
+    }
+    return (1);
+}
+
 int main (int argc, char **argv)
 {
     Server          serv;
     std::string     password;
     int             port;
+    std::string     address;
 
-    if (argc != 3)
-        return (1);
-
-    password = argv[2];
-    if (password.empty())
+    if (argc != 4)
     {
-        std::cout << "Password can't be empty." << std::endl;
+        printError("Missing parameters. Expected: ./ircserv <IP address> <port> <password>.");
         return (1);
     }
-    if (password.length() > 510)
+
+    password = argv[3];
+    if (password.length() < 0 || password.length() > 510)
     {
-        std::cout << "Password can't be longer than 510 chars (UTF-8)." << std::endl;
+        printError("Password can't be empty nor longer than 510 chars (UTF-8).");
         return (1);
     }
     serv.setPassword(password);
 
-    port = atoi(argv[1]);
-    if (port < 0 || port > 65535)
+    try
     {
-        std::cout << "Invalid port number. Available ports range from 0 to 65535. Suggested port: 6667" << std::endl;
+        port = ft_stoi(argv[2]);
+        if (port < 0 || port > 65535)
+        {
+            printError("Invalid port number. Available ports for IRC range from 6665 to 6669. Suggested port: 6667");
+            return (1);
+        }
+    }
+    catch(const std::exception& e)
+    {
+        printError("Invalid port number. Available ports for IRC range from 6665 to 6669. Suggested port: 6667");
+        return (1);
+    }
+
+    address = argv[1];
+    if (!isAddressValid(address))
+    {
+        printError("Invalid address. Expected V4 (ex: 127.0.0.1).");
         return (1);
     }
 
@@ -66,10 +102,11 @@ int main (int argc, char **argv)
     sigaction(SIGINT, &sa, NULL);
     sigaction(SIGQUIT, &sa, NULL);
 
+    address = "0.0.0.0"; // par defaut, permet acces au serveur a toutes adresses IP qui peuvent se connecter a la mienne
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
     addr.sin_port = htons((u_short)port);
-    addr.sin_addr.s_addr = INADDR_ANY;
+    inet_pton(AF_INET, address.c_str(), &(addr.sin_addr));// addr.sin_addr.s_addr = INADDR_ANY; 
     memset(&(addr.sin_zero), 0, 8);
 
     serv.openSocket(&addr);
